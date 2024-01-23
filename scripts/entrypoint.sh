48dae60cfaee7498d9cf297386a7a6a2899e6120
jinti <shenjindi@fourz.cn>
2024-01-23 12:15:04 +0800

    force update fonts

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -e
-
-if [ -f /app/fonts/local.conf ]; then
-  mkdir -p ~/.config/fontconfig/ && ln -s /app/fonts/local.conf ~/.config/fontconfig/local.conf
-  fc-cache -fv
-fi
-
+fc-cache -fv
 /app/browserlify $@
 
